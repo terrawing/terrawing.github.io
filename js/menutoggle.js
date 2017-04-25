632cbfe99acc841ff41ae01b47c2d5cb5b79a2a0
terrawing <terrawing@gmail.com>
2017-04-25 04:22:44 -0400

    Fixed the popup names for icon links
    Fixed the popup names for icon links

--- js/menutoggle.js
+++ js/menutoggle.js
@@ -1,15 +1,38 @@
 $( document ).ready(function() {
+    //http://stackoverflow.com/questions/19397636/popover-delay-at-bootstrap-doesnt-work
+    
     //Tooltip Home
-    $("#popoverhome").popover({ trigger: "hover", container: ".homespacer" });
-    var popover = $("#popover").data("popover");
+    $("#popoverhome").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popoverhome").attr('data-content', 'Home').data("bs.popover");
     
-    setTimeout(function() {
-        $("#popoverhome").attr('data-content', 'Home');
-        var popover = $('#popoverhome').data("popover");
-        popover.setContent();
-        popover.$tip.addClass(popover.options.placement);
-    },2000);
+    /*
+    $('#popoverhome').on('shown.bs.popover', function() {
+        setTimeout(function() {
+            $('#popoverhome').popover('hide');
+        }, 300);
+    });*/
     
+    //Tooltip About
+    $("#popoverabout").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popoverabout").attr('data-content', 'About').data("bs.popover");
+    
+    //Tooltip Prototype
+    $("#popoverprototype").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popoverprototype").attr('data-content', 'Prototypes').data("bs.popover");
+    
+    //Tooltip Design
+    $("#popoverdesign").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popoverdesign").attr('data-content', 'Designs').data("bs.popover");
+    
+    //Tooltip Coding
+    $("#popovercoding").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popovercoding").attr('data-content', 'Coding').data("bs.popover");
+    
+    //Tooltip Contact
+    $("#popovercontact").popover({ trigger: "hover", container: ".homespacer", delay: { show: "500", hide: "200" } });
+    $("#popovercontact").attr('data-content', 'Contact').data("bs.popover");
+    
+   
     // Closes the sidebar menu
     $("#menu-close").click(function(e) {
         e.preventDefault();
