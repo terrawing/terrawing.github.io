1c52463b8120ea90b7ed4ce1cf5bb4c8ae1ba355
terrawing <terrawing@gmail.com>
2017-04-25 02:53:48 -0400

    Updated April 25
    Updated April 25 version

--- js/menutoggle.js
+++ js/menutoggle.js
@@ -1,21 +1,31 @@
 $( document ).ready(function() {
-
-        // Closes the sidebar menu
-        $("#menu-close").click(function(e) {
-            e.preventDefault();
-            $("#sidebar-wrapper").toggleClass("active");
-        });
-        // Opens the sidebar menu
-        $("#menu-toggle").click(function(e) {
-            e.preventDefault();
-            $("#sidebar-wrapper").toggleClass("active");
-        });
-        
-        
-        //#to-top button appears after scrolling
+    //Tooltip Home
+    $("#popoverhome").popover({ trigger: "hover", container: ".homespacer" });
+    var popover = $("#popover").data("popover");
+    
+    setTimeout(function() {
+        $("#popoverhome").attr('data-content', 'Home');
+        var popover = $('#popoverhome').data("popover");
+        popover.setContent();
+        popover.$tip.addClass(popover.options.placement);
+    },2000);
+    
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
+    
+    
+    //#to-top button appears after scrolling
     var fixed = false;
     $(document).scroll(function() {
-        if ($(this).scrollTop() > 250) {
+        if ($(this).scrollTop() > 50) {
             if (!fixed) {
                 fixed = true;
                 // $('#to-top').css({position:'fixed', display:'block'});
