a30cd6cd4992f18e8ed465cf8b7488d4eb6ceb2b
William Wong <zakura_media@hotmail.com>
2021-02-19 19:15:11 -0500

    0.62 - Added GSAP animation to the homepage

---
+++ js/gsaphome.js
@@ -0,0 +1,12 @@
+gsap.from(".bar1", {delay: 1, duration: 1.5, scaleX: 0.25, transformOrigin: 'left', yoyo: true, repeat: -1, ease: 'power1.out'});
+gsap.from(".bar2", {delay: 2, duration: 1.75, scaleX: 0.25, transformOrigin: 'left', yoyo: true, repeat: -1, ease: 'power1.out'}, "=.25");
+gsap.from(".bar3", {delay: 3, duration: 2, scaleX: 0.25, transformOrigin: 'left', yoyo: true, repeat: -1, ease: 'power1.out'}, "=.5");
+
+var tl = gsap.timeline({repeat: -1, repeatDelay: 1.25});
+
+tl.to(".key1", {duration: 0.75, fill: '#023047', yoyo: true, ease: "none"});
+tl.to(".key1", {duration: 0.75, fill: '#FFFFFF', yoyo: true, ease: "none"});
+tl.to(".key2", {duration: 0.75, fill: '#023047', yoyo: true, ease: "none"});
+tl.to(".key2", {duration: 0.75, fill: '#FFFFFF', yoyo: true, ease: "none"});
+tl.to(".key3", {duration: 0.75, fill: '#023047', yoyo: true, ease: "none"});
+tl.to(".key3", {duration: 0.75, fill: '#FFFFFF', yoyo: true, ease: "none"});
